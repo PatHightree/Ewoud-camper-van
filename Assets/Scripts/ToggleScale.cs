b81aaa92bd1a96ff8c16d3c72324f8d8a263cd42
Patrick Hogenboom <patrickhightree@gmail.com>
2020-12-04 20:24:41 +0100

    Added scale toggle script
    Press space to toggle VW transporter size between L1 and L2 dimensions

--- Assets/Scripts/ToggleScale.cs
+++ Assets/Scripts/ToggleScale.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class ToggleScale : MonoBehaviour
 {
+    public Transform Target;
+    public Vector3 Scale1;
+    public Vector3 Scale2;
+
+    private Vector3 m_initScale;
+    private bool m_isScaled;
+    
     void Start()
     {
-        
+        m_initScale = Target.localScale;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            Toggle();
+    }
+    
+    public void Toggle()
+    {
+        if (m_isScaled)
+            Target.localScale = m_initScale;
+        else
+            Target.localScale = new Vector3(
+                ScaleComponent(m_initScale.x, Scale1.x, Scale2.x),
+                ScaleComponent(m_initScale.y, Scale1.y, Scale2.y),
+                ScaleComponent(m_initScale.z, Scale1.z, Scale2.z)
+                );
+        m_isScaled = !m_isScaled;
+    }
+
+    float ScaleComponent(float _init, float _scale1, float _scale2)
+    {
+        return _init / _scale1 * _scale2;
     }
 }
